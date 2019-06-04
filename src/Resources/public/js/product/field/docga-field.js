b1a96c5a79ff0ae081fd8a6d35184f1123b5043c
Francois BOUYER <francois@oniti.fr>
2019-06-04 08:33:23 +0200

    Corrections sur docga-field

--- src/Resources/public/js/product/field/docga-field.js
+++ src/Resources/public/js/product/field/docga-field.js
@@ -10,12 +10,20 @@ define([
     fieldTemplate
 ) {
     return Field.extend({
-        fieldTemplate: _.template(fieldTemplate),
+        template: _.template(fieldTemplate),
         events: {
             'change .field-input:first input[type="range"]': 'updateModel'
         },
         renderInput: function (context) {
-            this.$el.html(fieldTemplate)
+            this.$el.html(this.template({
+                base_url: this.getFormData()['docga_extended_attribute_type___base_url'] ?
+                    this.getFormData()['docga_extended_attribute_type___base_url'].value : '',
+                api_key: this.getFormData()['docga_extended_attribute_type___api_key'] ?
+                    this.getFormData()['docga_extended_attribute_type___api_key'].value : '',
+                api_secret: this.getFormData()['docga_extended_attribute_type___api_secret'] ?
+                    this.getFormData()['docga_extended_attribute_type___api_secret'].value : '',
+            }));
+
             return this;
         },
         updateModel: function () {
